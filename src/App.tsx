3fc51b005ddbd23ef0353863b296c13a11ee0235
ZeyadMashhour <91096543+ZeyadMashhour@users.noreply.github.com>
2025-08-16 21:13:03 +0300

    push ya zeyad bassel said

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import Header from "./components/Header/Header";
 import PrayerTimes from "./components/PrayerTimes/PrayerTimes";
 import Azkar from "./components/Azkar/Azkar";
@@ -14,14 +12,14 @@ const App = () => {
       <main className="flex-1 w-full px-4 py-2">
         <div className="flex flex-col md:flex-row w-full gap-4">
           <div className="w-full md:w-1/2">
-            <PrayerTimes className="w-full h-full" />
+            <PrayerTimes/>
           </div>
           <div className="w-full md:w-1/2">
-            <Azkar className="w-full h-full" />
+            <Azkar/>
           </div>
         </div>
       </main>
-      
+
       {/* Footer */}
     </div>
   );
--- src/components/Azkar/Azkar.tsx
+++ src/components/Azkar/Azkar.tsx
@@ -1,15 +1,19 @@
-import styles from "./Azkar.module.css";
-const Azkar = () => {
+const Azkar= () => {
   return (
-    <div className={styles.azkar}>
-      <h2 className={styles["azkar-title"]}>أذكار الصباح و المساء</h2>
-      <div className={styles["azkar-nav"]}>
-        <button>أذكار الصباح</button>
-        <span> | </span>
-        <button>أذكار المساء</button>
+    <div className="bg-purple border-2 border-gold rounded-2xl shadow-md p-6 flex flex-col items-center text-center">
+      <h2 className="text-gold text-2xl font-bold mb-4">
+        أذكار الصباح و المساء
+      </h2>
+      <div className="flex flex-col gap-4 w-full max-w-xs">
+        <button className="bg-darkPurple text-white py-2 px-4 rounded-xl border border-gold hover:bg-green transition">
+          أذكار الصباح
+        </button>
+
+        <button className="bg-darkPurple text-white py-2 px-4 rounded-xl border border-gold hover:bg-green transition">
+          أذكار المساء
+        </button>
       </div>
     </div>
   );
-};
-
+}
 export default Azkar;
