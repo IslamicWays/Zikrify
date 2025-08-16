d56f392f8effa706fad93a1d09212ef8bf630129
ZeyadMashhour <91096543+ZeyadMashhour@users.noreply.github.com>
2025-08-16 18:55:04 +0300

    added colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+       colors: {
+        background: "#000000",   // black background
+        card: "#1C1C1C",         // dark gray card background
+        accent: "#F6C90E",       // gold/yellow highlight
+        text: "#FFFFFF",         // main white text
+        secondary: "#B0B0B0",    // gray secondary text
+        success: "#22C55E",      // green highlight
+      },
+    },
   },
   plugins: [],
 };
